2ba19f99a334cb360b54a37f12f7f0ca2ea00a53
hsani24 <hsani24@gmail.com>
2023-08-25 11:58:47 +0200

    Initial Commit

---
+++ FirebaseAPPNewUITests/FirebaseAPPNewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FirebaseAPPNewUITestsLaunchTests.swift
+//  FirebaseAPPNewUITests
+//
+//  Created by Sani I on 25.08.23.
+//
+
+import XCTest
+
+final class FirebaseAPPNewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
